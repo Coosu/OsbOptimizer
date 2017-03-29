e65b7776815bd366a0c6519f733800426799ca09
Milkitic <Milkitic@126.com>
2017-03-29 13:11:09 +0800

    Improved check rules.

--- osb optimizer/osb optimizer/OSBOptimizer/ChkRules.cs
+++ osb optimizer/osb optimizer/OSBOptimizer/ChkRules.cs
@@ -55,16 +55,166 @@ namespace OsuStoryboard.OSBOptimizer
 
             }
         }
+        private static void chkS(sbObject sb)
+        {
+            int?[] start, end;
+
+            if (sb.Scale.Count != 0)
+            {
+
+                List<int?> tmpstart = sb.Scale.starttime_L.ToList();
+                tmpstart.Sort();
+
+                if (IsEqual(tmpstart, sb.Scale.starttime_L)) tmpstart = null;
+                else
+                {
+                    start = sb.Scale.starttime_L.ToArray();
+                    end = sb.Scale.endtime_L.ToArray();
+                    var line = sb.Scale.S.ToArray();
+                    Sort(start, end, line);
+
+                    sb.Scale.S = new List<Scale>(line);
+                }
+
+            }
+        }
+        private static void chkR(sbObject sb)
+        {
+            int?[] start, end;
+
+            if (sb.Rotate.Count != 0)
+            {
+
+                List<int?> tmpstart = sb.Rotate.starttime_L.ToList();
+                tmpstart.Sort();
+
+                if (IsEqual(tmpstart, sb.Rotate.starttime_L)) tmpstart = null;
+                else
+                {
+                    start = sb.Rotate.starttime_L.ToArray();
+                    end = sb.Rotate.endtime_L.ToArray();
+                    var line = sb.Rotate.R.ToArray();
+                    Sort(start, end, line);
+
+                    sb.Rotate.R = new List<Rotate>(line);
+                }
+
+            }
+        }
+        private static void chkC(sbObject sb)
+        {
+            int?[] start, end;
+
+            if (sb.Color.Count != 0)
+            {
+
+                List<int?> tmpstart = sb.Color.starttime_L.ToList();
+                tmpstart.Sort();
+
+                if (IsEqual(tmpstart, sb.Color.starttime_L)) tmpstart = null;
+                else
+                {
+                    start = sb.Color.starttime_L.ToArray();
+                    end = sb.Color.endtime_L.ToArray();
+                    var line = sb.Color.C.ToArray();
+                    Sort(start, end, line);
+
+                    sb.Color.C = new List<Color>(line);
+                }
+
+            }
+        }
+        private static void chkMX(sbObject sb)
+        {
+            int?[] start, end;
+
+            if (sb.MoveX.Count != 0)
+            {
+
+                List<int?> tmpstart = sb.MoveX.starttime_L.ToList();
+                tmpstart.Sort();
+
+                if (IsEqual(tmpstart, sb.MoveX.starttime_L)) tmpstart = null;
+                else
+                {
+                    start = sb.MoveX.starttime_L.ToArray();
+                    end = sb.MoveX.endtime_L.ToArray();
+                    var line = sb.MoveX.MX.ToArray();
+                    Sort(start, end, line);
+
+                    sb.MoveX.MX = new List<MoveX>(line);
+                }
+
+            }
+        }
+        private static void chkMY(sbObject sb)
+        {
+            int?[] start, end;
+
+            if (sb.MoveY.Count != 0)
+            {
+
+                List<int?> tmpstart = sb.MoveY.starttime_L.ToList();
+                tmpstart.Sort();
+
+                if (IsEqual(tmpstart, sb.MoveY.starttime_L)) tmpstart = null;
+                else
+                {
+                    start = sb.MoveY.starttime_L.ToArray();
+                    end = sb.MoveY.endtime_L.ToArray();
+                    var line = sb.MoveY.MY.ToArray();
+                    Sort(start, end, line);
+
+                    sb.MoveY.MY = new List<MoveY>(line);
+                }
+
+            }
+        }
+        private static void chkV(sbObject sb)
+        {
+            int?[] start, end;
+
+            if (sb.Vector.Count != 0)
+            {
+
+                List<int?> tmpstart = sb.Vector.starttime_L.ToList();
+                tmpstart.Sort();
+
+                if (IsEqual(tmpstart, sb.Vector.starttime_L)) tmpstart = null;
+                else
+                {
+                    start = sb.Vector.starttime_L.ToArray();
+                    end = sb.Vector.endtime_L.ToArray();
+                    var line = sb.Vector.V.ToArray();
+                    Sort(start, end, line);
+
+                    sb.Vector.V = new List<Vector>(line);
+                }
+
+            }
+        }
 
         public static bool Check(sbObject sb)
         {
-            chkM(sb); chkF(sb);
-
+            chkM(sb); chkF(sb); chkS(sb); chkR(sb);
+            chkC(sb); chkMX(sb); chkMY(sb); chkV(sb);
 
             for (int i = 1; i < sb.Move.Count; i++)
                 if (sb.Move[i].StartTime < sb.Move[i - 1].EndTime) return false;
-            for (int i = 1; i < sb.Fade.Count; i++) 
+            for (int i = 1; i < sb.Fade.Count; i++)
                 if (sb.Fade[i].StartTime < sb.Fade[i - 1].EndTime) return false;
+            for (int i = 1; i < sb.Scale.Count; i++)
+                if (sb.Scale[i].StartTime < sb.Scale[i - 1].EndTime) return false;
+            for (int i = 1; i < sb.Rotate.Count; i++)
+                if (sb.Rotate[i].StartTime < sb.Rotate[i - 1].EndTime) return false;
+            for (int i = 1; i < sb.Color.Count; i++)
+                if (sb.Color[i].StartTime < sb.Color[i - 1].EndTime) return false;
+            for (int i = 1; i < sb.MoveX.Count; i++)
+                if (sb.MoveX[i].StartTime < sb.MoveX[i - 1].EndTime) return false;
+            for (int i = 1; i < sb.MoveY.Count; i++)
+                if (sb.MoveY[i].StartTime < sb.MoveY[i - 1].EndTime) return false;
+            for (int i = 1; i < sb.Vector.Count; i++)
+                if (sb.Vector[i].StartTime < sb.Vector[i - 1].EndTime) return false;
 
             return true;
         }
@@ -86,7 +236,7 @@ namespace OsuStoryboard.OSBOptimizer
             int j;
             while (low < high)
             {
-                for (j = low; j < high; ++j) //正向冒泡，找到最大者  
+                for (j = low; j < high; ++j) //正向冒泡，找到最大者
                     if (start[j] > start[j + 1])
                     {
                         tmp = start[j]; start[j] = start[j + 1]; start[j + 1] = tmp;
